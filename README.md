e27f02dc0ec1be8608212e6e5175cf195b09a132
Ron Korving <rkorving@wizcorp.jp>
2011-11-09 16:27:21 +0900

    * First version.

---
+++ README.md
@@ -0,0 +1,105 @@
+# LocalCache
+
+LocalCache is a fast, minimalistic in-memory key/value cache, with support for expiration.
+In essence, it's quite similar to other key/value stores like memcached, except that LocalCache
+only keeps things in the current process's memory, and not in some external service.
+
+## Why would I use this?
+
+Sometimes you want to store some calculated, or externally retrieved values, because you know
+you may need them again very soon. Nonetheless, you don't want to keep them around for too long.
+That's where the ability to expire information comes in handy. LocalCache has its own garbage
+collection mechanism for expiring old data. The main design considerations are a small
+performance overhead, and a fast garbage collection mechanism.
+
+## API
+
+LocalCache exposes the following APIs:
+
+### new LocalCache([cycleInterval], [options]) (constructor)
+
+`cycleInterval` is the garbage collection cycle interval, in seconds. If you tend to store a lot
+of data, with short TTL values, it is recommended to use a relatively low interval. If data
+generally has a high TTL, you may want to raise the interval.
+
+`options` is an object for passing options to the cache. The following values are supported:
+
+`maxKeys` (integer, default: null). Can be set to limit the total amount of values that may be
+stored in the cache. If this value gets exceeded, the values that will expire soonest will be made
+to expire prematurely.
+
+`aggressiveExpiration` (boolean, default: false). If set to true, even if a get() call yields a value,
+and that value is expired (just not yet garbage collected), it will not be returned.
+
+### cache.flush()
+
+Resets the entire cache to zero.
+
+### cache.diagnostics()
+
+Returns an object containing information about the state of the cache.
+
+### cache.add(key, value, [ttl])
+
+Adds a value to the cache, optionally expiring after `ttl` seconds. If a value with that key already
+exists, it will not be overwritten and false will be returned. True will be returned otherwise.
+
+### cache.set(key, value, [ttl])
+
+Adds a value to the cache, optionally expiring after `ttl` seconds. If a value with that key already
+exists, it will be overwritten.
+
+### cache.del(key)
+
+Deletes the value with key `key` from the cache. If the value existed, true will be returned.
+False otherwise.
+
+### cache.get(key)
+
+Returns the value stored by key `key`. If it does not exist, undefined will be returned.
+
+### cache.getExpirationTime(key)
+
+Yields the time (unix timestamp in seconds) at which the value for key `key` will expire. if the value
+will never expire, null is returned. If the value does not exist, undefined is returned.
+
+### cache.touch(key, ttl)
+
+Resets the value's expiration time to now + ttl seconds.
+
+
+## Example
+
+``` javascript
+var LocalCache = require('localcache').LocalCache;
+
+// instantiate
+
+var options = {};
+
+var cache = new LocalCache(60, options);
+
+// set key "hello" to value "world" and expire after 30 seconds.
+
+cache.set('hello', 'world', 30);
+
+// adding a key called "hello" will now fail (return false).
+
+cache.add('hello', 'foo');
+
+// print "hello world"
+
+console.log('hello', cache.get('hello'));
+
+// extend expiration to 120 seconds after now
+
+cache.touch('hello', 120);
+
+// cleanup prematurely, because we can
+
+cache.del('hello');
+```
+
+## License
+
+
---
+++ index.js
@@ -0,0 +1,243 @@
+var now = function () {
+	return (Date.now() / 1000) >>> 0;
+};
+
+
+exports.setTimestampProvider = function (fn) {
+	now = fn;
+};
+
+
+function LocalCache(resolution, options) {
+	// options:
+	//   maxKeys: int (null)                 (if there are more keys stored than allowed, GC cycles will be triggered early).
+	//   aggressiveExpiration: bool (false)  (when true, a get() will not return an expired value, even if it still hasn't been garbage collected).
+
+	this.resolution = resolution || 60; 	// in seconds, eg: 60
+	this.options = options || {};
+
+	this.store = {};	// key: value
+	this.expiry = [];
+	this.keyCount = 0;
+
+	this.lastGcCycleTime = now();
+
+	this.scheduleGcCycle(this.resolution);
+}
+
+
+exports.LocalCache = LocalCache;
+
+
+LocalCache.prototype.flush = function () {
+	this.store = {};
+	this.expiry = [];
+	this.keyCount = 0;
+};
+
+
+LocalCache.prototype.diagnostics = function () {
+	var nextExpirationKeys = this.expiry[0] ? this.expiry[0].length : 0;
+
+	return {
+		keys: this.keyCount,
+		expirationBlocks: this.expiry.length,
+		resolution: this.resolution,
+		nextExpiration: { keys: nextExpirationKeys, time: this.lastGcCycleTime + this.resolution }
+	};
+};
+
+
+LocalCache.prototype.gcCycle = function () {
+	var keys = this.expiry.shift();
+
+	if (keys) {
+		for (var i = 0, len = keys.length; i < len; i++) {
+			var key = keys[i];
+
+			if (this.store.hasOwnProperty(key)) {
+				delete this.store[key];
+				this.keyCount--;
+			}
+		}
+
+		return true;
+	}
+
+	return false;
+};
+
+
+LocalCache.prototype.scheduleGcCycle = function (interval) {
+	var that = this;
+
+	setTimeout(function () {
+		that.gcScheduled();
+	}, interval * 1000);
+};
+
+
+LocalCache.prototype.gcScheduled = function () {
+	var currentTime = this.lastGcCycleTime + this.resolution;
+
+	this.gcCycle();
+
+	this.lastGcCycleTime = currentTime;
+
+	var interval = this.lastGcCycleTime + this.resolution - now();
+	if (interval <= 0) {
+		interval = 1;
+	}
+
+	this.scheduleGcCycle(interval);
+};
+
+
+LocalCache.prototype.calcBlockIndex = function (expirationTime) {
+	return Math.floor((expirationTime - this.lastGcCycleTime) / this.resolution);
+};
+
+
+LocalCache.prototype.removeFromExpiryBlock = function (key, blockIndex) {
+	var block = this.expiry[blockIndex];
+	if (block) {
+		var index = block.indexOf(key);
+		if (index !== -1) {
+			block.splice(index, 1);
+		}
+	}
+};
+
+
+LocalCache.prototype.addToExpiryBlock = function (key, blockIndex) {
+	var block = this.expiry[blockIndex];
+	if (block) {
+		block.push(key);
+	} else {
+		this.expiry[blockIndex] = [key];
+	}
+};
+
+
+LocalCache.prototype.touch = function (key, ttl) {
+	var value = this.store[key];
+	if (value) {
+		var oldBlock = null, newBlock = null, expirationTime = null;
+
+		if (value[1]) {
+			oldBlock = this.calcBlockIndex(value[1]);
+		}
+
+		if (ttl > 0) {
+			expirationTime = now() + ttl;
+			newBlock = this.calcBlockIndex(expirationTime);
+		}
+
+		if (oldBlock !== newBlock) {
+			if (oldBlock !== null) {
+				this.removeFromExpiryBlock(key, oldBlock);
+			}
+
+			if (newBlock !== null) {
+				this.addToExpiryBlock(key, newBlock);
+			}
+		}
+
+		if (expirationTime === null) {
+			// no expiration
+
+			delete value[1];
+		} else {
+			value[1] = expirationTime;
+		}
+
+		return true;
+	}
+
+	return false;
+};
+
+
+LocalCache.prototype.del = function (key) {
+	var value = this.store[key];
+	if (value) {
+		if (value[1]) {
+			// we have to remove it from the block, otherwise we would be removing it at a later time as well (not desirable)
+
+			var expiryBlock = this.calcBlockIndex(value[1]);
+
+			this.removeFromExpiryBlock(key, expiryBlock);
+		}
+
+		delete this.store[key];
+		this.keyCount--;
+
+		return true;
+	}
+
+	return false;
+};
+
+
+LocalCache.prototype.get = function (key) {
+	var value = this.store[key];
+	if (value) {
+		if (this.options.aggressiveExpiration && value[1] && value[1] < now()) {
+			this.del(key);
+		} else {
+			return value[0];
+		}
+	}
+};
+
+
+LocalCache.prototype.getExpirationTime = function (key) {
+	// returns null if it doesn't expire, false if key not found
+
+	var value = this.store[key];
+	if (value) {
+		return value[1] || null;
+	}
+};
+
+
+LocalCache.prototype.add = function (key, value, ttl) {
+	if (this.store.hasOwnProperty(key)) {
+		return false;
+	}
+
+	if (this.options.maxKeys) {
+		while (this.keyCount > 0 && this.keyCount >= this.options.maxKeys) {
+			// early GC cycle
+
+			if (!this.gcCycle()) {
+				// nothing to cycle
+				break;
+			}
+		}
+	}
+
+	value = [value];
+
+	if (ttl > 0) {
+		var expirationTime = now() + ttl;
+
+		value.push(expirationTime);
+
+		var expiryBlock = this.calcBlockIndex(expirationTime);
+
+		this.addToExpiryBlock(key, expiryBlock);
+	}
+
+	this.store[key] = value;
+	this.keyCount++;
+
+	return true;
+};
+
+
+LocalCache.prototype.set = function (key, value, ttl) {
+	this.del(key);
+	this.add(key, value, ttl);
+};
+
